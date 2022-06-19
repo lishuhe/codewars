66d879afee91af1f86bbb96b78002d2137e76f5a
Lishu <37666874+lishuhe@users.noreply.github.com>
2022-06-19 01:04:32 -0700

    Update credit card mask.py

--- credit card mask.py
+++ credit card mask.py
@@ -1,15 +1,27 @@
-def unique_in_order(test):
-    result = []
-    if len(test) > 1:
-        for i in range(len(test)-1):
-            if test[i] != test[i+1]:
-                result.append(test[i])
-        result.append(test[len(test)-1])
-    elif len(test) == 1:
-        result.append(test)
+# Usually when you buy something, you're asked whether your credit card number,
+# phone number or answer to your most secret question is still correct.
+# However, since someone could look over your shoulder, you don't want that shown on your screen.
+# Instead, we mask it.
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+# "4556364607935616" --> "############5616"
+#    "64607935616" -->      "#######5616"
+#              "1" -->                "1"
+#               "" -->                 ""
+
+# "What was the name of your first pet?"
+# "Skippy" --> "##ippy"
+
+# "Nananananananananananananananana Batman!" --> "####################################man!"
+
+def maskify(cc):
+    num = 0
+    a = 0
+    if len(cc) > 4:
+        a = '#' * (len(cc)-4)
+        return (a + cc[len(cc)-4: len(cc)+1])
     else:
-        result = []
-    return result
+        return (cc)
 
 #test case
-print(unique_in_order('hello world'))
+print(maskify('hello world'))
