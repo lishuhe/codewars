03f90a46bc658b8d2f390972367bc38155af0531
Lishu <37666874+lishuhe@users.noreply.github.com>
2022-07-18 14:23:53 -0700

    Add files via upload

---
+++ Create Phone Number.py
@@ -0,0 +1,9 @@
+# Write a function that accepts an array of 10 integers (between 0 and 9),
+# that returns a string of those numbers in the form of a phone number.    
+    
+def create_phone_number(n):
+    return '('+"".join(map(str, n[0:3]))+')'+' '+ "".join(map(str, n[3:6]))+ '-' + "".join(map(str, n[6:10]))
+    
+# test cases
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]))
